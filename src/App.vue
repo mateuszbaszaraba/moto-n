386067a0d32f66a509b7dcbae6a118d5fcd7cd4d
Matt <mateuszbaszaraba@outlook.com>
2020-07-10 14:59:54 +0200

    added vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="container">
     <div class="row">
-      
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-8 offset-lg-2 mt-5">
+        <h1>Home</h1>
+      </div>
     </div>
   </div>
 </template>
