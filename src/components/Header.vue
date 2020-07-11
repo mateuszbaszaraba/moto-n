cdc81d6163312bb4ac5b1cc109ceb7f8e220c03c
Matt <mateuszbaszaraba@outlook.com>
2020-07-11 21:57:38 +0200

    improved header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -22,17 +22,27 @@
 
         <div class="nav navbar-nav navbar-right">
             <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
+                    <a @click="isDropdownOpen = !isDropdownOpen" class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
+                    Save & Load
                     </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
+                    <div class="dropdown-menu" :class="{show: isDropdownOpen}" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Something else here</a>
                     </div>
                 </li>
         </div>
 
     </nav>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                isDropdownOpen: false
+            }
+        }
+    }
+</script>
