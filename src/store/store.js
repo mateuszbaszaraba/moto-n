adee8608f67d6482f7cca8e2c2b814d230f7c2c1
Matt <mateuszbaszaraba@outlook.com>
2020-07-12 20:49:42 +0200

    added essential files

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import car from './car';
+import car from './modules/cars';
+import profile from './modules/profile'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     modules: {
-        car
+        car,
+        profile
     }
 })
